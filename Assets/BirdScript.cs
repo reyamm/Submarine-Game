3a8956e41baf2b391aeb2b4f91af1064dde2eb62
reyamm <zririb2@gmail.com>
2025-09-20 21:15:07 +0300

    Gave it a name

--- Assets/BirdScript.cs
+++ Assets/BirdScript.cs
@@ -5,7 +5,7 @@ public class BirdScript : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        gameObject.name = "Submarine";
     }
 
     // Update is called once per frame
