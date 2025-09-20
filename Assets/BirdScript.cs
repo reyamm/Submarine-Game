bfd5a5d233d83e631b098e40043818821b36d5e4
reyamm <zririb2@gmail.com>
2025-09-20 21:27:35 +0300

    Update BirdScript and scene changes

--- Assets/BirdScript.cs
+++ Assets/BirdScript.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class BirdScript : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public Rigidbody2D myRigidbody;
     void Start()
     {
     
@@ -11,6 +12,6 @@ public class BirdScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        myRigidbody.linearVelocity = Vector2.up * 10;
     }
 }
